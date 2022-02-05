c7b1284effa81fdde85a8740fc6b5bd38efed2a1
codekissyoung <1162097842@qq.com>
2022-02-05 17:24:01 +0800

    update java

--- counter-app/app.py
+++ counter-app/app.py
@@ -5,6 +5,7 @@ from flask import Flask
 app = Flask(__name__)
 cache = redis.Redis(host='redis', port=6379)
 
+
 def get_hit_count():
     retries = 5
     while True:
@@ -16,10 +17,12 @@ def get_hit_count():
             retries -= 1
             time.sleep(0.5)
 
+
 @app.route('/')
 def hello():
     count = get_hit_count()
     return "What's up Docker Deep Divers! You've visited me {} times.\n".format(count)
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", debug=True)
--- linkos/run.sh
+++ linkos/run.sh
@@ -1,5 +1,2 @@
 #!/bin/bash
 docker build -t linkos .
-
-
-
